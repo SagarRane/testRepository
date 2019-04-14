ecffbcd456f1a9763dfeb760d80fbc18a260d85e
SagarRane <48590491+SagarRane@users.noreply.github.com>
2019-04-14 13:07:40 +0530

    Demo trigger build after another build

--- script.sh
+++ script.sh
@@ -4,3 +4,4 @@ echo "[INFO] Running script available in GitHub via Jenkins"
 
 echo "[INFO] Demo for running script"
 echo "[INFO] Demo for artifacts archives"
+echo "[INFO] Demo trigger build after another build"
